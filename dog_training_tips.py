13edf50643165c934845ce0ec5d141226e86f537
alyssa-dannielle <103299483+alyssa-dannielle@users.noreply.github.com>
2022-04-24 17:37:15 -0400

    Add files via upload
    I'm just throwing it here while it sits on the back burner

---
+++ dog_training_tips.py
@@ -0,0 +1,10 @@
+# Alyssa Miller alyssa.dannielle@gmail.com
+# prints random dog training tips - these are just things ive noticed while training my pup
+
+print("Have a nice variety of treats ranging in value.") # use high value for "come"
+
+print('Have a (cue?) word like "yes!" or "good boy!"') # only use while giving treat, then they associate the phrase with a treat
+
+print("Always positive reinforcement forever") # if you notice a behavior you like, give a treat
+
+print("Always keep treats on hand") # you want these to be low value
